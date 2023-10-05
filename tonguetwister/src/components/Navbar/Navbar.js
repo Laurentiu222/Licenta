cc64431d7439e4a9053d73c2bc81847de6fb2a5d
Laurentiu222 <125998865+Laurentiu222@users.noreply.github.com>
2023-10-06 01:02:26 +0300

    Nav

--- tonguetwister/src/components/Navbar/Navbar.js
+++ tonguetwister/src/components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ function Navbar(){
           </div>
           <ul className={click ? 'nav-menu active': 'nav-menu'}>
             <li className="nav-item">
-              <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+              <Link to='/Home' className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
