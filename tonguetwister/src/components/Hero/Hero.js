946da94f878726284d5c2294956de5e4a2816c6b
Laurentiu222 <125998865+Laurentiu222@users.noreply.github.com>
2023-10-07 13:39:34 +0300

    hero

--- tonguetwister/src/components/Hero/Hero.js
+++ tonguetwister/src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ export function Hero(){
 
   return(
     <div className="hero-container">
-      <video src="\video\production_id_4878177 (1080p).mp4" autoPlay loop muted/>
+      <video src="\video\earth.mp4" autoPlay loop muted/>
       <h1>Unlocking the World of English</h1>
       <div className="hero--btns">
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>Get Started </Button>
