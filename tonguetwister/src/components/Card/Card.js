dc30c46008b051d145a12fc65ed57aae04346fa8
Laurentiu222 <125998865+Laurentiu222@users.noreply.github.com>
2023-10-09 00:24:25 +0300

    card

--- tonguetwister/src/components/Card/Card.js
+++ tonguetwister/src/components/Card/Card.js
@@ -4,14 +4,14 @@ import './Card.css';
 
 export default function Card() {
   return (
-    <div className='cards'>
-      <h1>Learning</h1>
-      <div className='cards--container'>
+    <div className='card'>
+      <h1>First steps in to becoming a true British Person</h1>
+      <div className='card--container'>
         <div className='wrapper'>
-          <ul className='cards--items'>
+          <ul className='card--items'>
             <CardItem
               src="img/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
-              text="Explore a new side of Languages"
+              text="Explore a new side of a new Language"
               label='Adventure'
               path='/Learn'
             />
