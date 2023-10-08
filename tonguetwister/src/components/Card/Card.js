ff1f4498d72d78acbda6a47a83cebabd83761a6c
Laurentiu222 <125998865+Laurentiu222@users.noreply.github.com>
2023-10-09 00:46:10 +0300

    card

--- tonguetwister/src/components/Card/Card.js
+++ tonguetwister/src/components/Card/Card.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import CardItem from './CardItem';
 import './Card.css';
+import CardItem from './CardItem';
 
-export default function Card() {
+function Card() {
   return (
-    <div className='card'>
+    <div className='cards'>
       <h1>First steps in to becoming a true British Person</h1>
-      <div className='card--container'>
-        <div className='wrapper'>
-          <ul className='card--items'>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
             <CardItem
-              src="img/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
-              text="Explore a new side of a new Language"
-              label='Adventure'
+              src='img\janko-ferlic-sfL_QOnmy00-unsplash.jpg'
+              text='Explore a new side of a new Language'
+              label='Learning'
               path='/Learn'
             />
             <CardItem
-              src="img/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
-              text="Explore a new side of Languages"
-              label='Adventure'
-              path='/Learn'
+              src='img\jessica-pamp-2XeP0c-J5B4-unsplash.jpg'
+              text='Travel through challanges and win a badge'
+              label='Quizz'
+              path='/Quizz'
             />
             <CardItem
-              src="img/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
-              text="Explore a new side of Languages"
-              label='Adventure'
-              path='/Learn'
+              src='img\blake-cheek-YJ1HGUZc668-unsplash.jpg'
+              text='Every riddler has an answer'
+              label='Dictionary'
+              path='/Dictionary'
             />
           </ul>
+          
         </div>
       </div>
     </div>
   );
 }
+
+export default Card;
--- tonguetwister/src/components/Card/CardItem.js
+++ tonguetwister/src/components/Card/CardItem.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function CardItem(props) {
+function CardItem(props) {
   return (
-    <li className='card-item'>
-      <Link className='card-item-link' to={props.path}>
-        <figure className='card-item-pic-wrap'>
-          <img src={props.src} className='card-item-img' alt={props.label} />
-        </figure>
-        <div className='card-item-info'>
-          <h5 className='card-item-text'>{props.text}</h5>
-        </div>
-      </Link>
-    </li>
+    <>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            <img
+              className='cards__item__img'
+              alt=''
+              src={props.src}
+            />
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </>
   );
 }
+
+export default CardItem;
